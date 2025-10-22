a8ac3a6d1631ed2a07ff2fa203618f1e4a506585
Aaron Wu <awu0626@seas.upenn.edu>
2025-10-21 23:12:00 -0400

    added header for Linear search

---
+++ algorithms/LinearSearch/LinearSearch.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "LinearSearch.h"
+
+using namespace std;
+
+int main() {
+	cout << "hi" << endl;
+	return 0;
+}
---
+++ algorithms/LinearSearch/LinearSearch.h
@@ -0,0 +1,8 @@
+#ifdef LINEARSEARCH_H
+#define LINEARSEARCH_H
+
+
+int LinearSearch(int[] arr, int target);
+
+
+#endif
